56a4771d381c0398abdf014d488989dad8c9367f
pingwen <pingwen@chelun.com>
2020-04-22 16:27:37 +0800

    v1.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './component/app/app';
 
 function init() {
-  const a = 5;
+  const a = 6;
   ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
 }
 init();
